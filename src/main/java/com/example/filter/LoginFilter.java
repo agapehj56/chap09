1142a67e99a2e821ba56d529ce984f7c85e675d5
hjmin <agapehj.min@gmail.com>
2017-02-21 16:03:07 +0900

    practice jsp cookies

---
+++ src/main/java/com/example/filter/LoginFilter.java
@@ -0,0 +1,68 @@
+package com.example.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+@WebFilter("/admin/*")
+public class LoginFilter implements Filter {
+
+	static Log log = LogFactory.getLog(LoginFilter.class);
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		log.info("##############");
+		log.info("doFilter()");
+		log.info("##############");
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse resp = (HttpServletResponse) response;
+		
+		Cookie[] cookies = req.getCookies();
+		
+		boolean login = false;
+		
+		for(Cookie c : cookies) {
+			String name = c.getName();
+			if("login".equals(name))
+				login = true;
+		}
+		
+//		boolean anonymous = false;
+//		String path = req.getRequestURI();
+//		log.info("path=" + path);
+//		if(path.indexOf("/login") >= 0) anonymous = true;
+//		if(path.indexOf("/webjars") >= 0 ) anonymous = true;
+		
+		if(login)
+			chain.doFilter(request, response);
+		else 
+			resp.sendRedirect("/login");
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
